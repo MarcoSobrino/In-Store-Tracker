32186be85730a3a51488bda40ec4cbf5d70b765b
JonasXIII <jonasloeser@gmail.com>
2023-11-19 14:11:11 -0800

    added convertion to heatmap

--- LiveTracking/dashboard/database_tables.py
+++ LiveTracking/dashboard/database_tables.py
@@ -1,16 +1,16 @@
 import sqlite3
-
+print("starting")
 # Connect to the database
 conn = sqlite3.connect('LiveTracking.db')
 
 # Create a cursor object using the cursor() method
 cursor = conn.cursor()
-
+print("made it here")
 # Retrieve all tables in the database
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
 tables = cursor.fetchall()
-
+print("Printing table details")
 for table in tables:
     table_name = table[0]
     print(f"Table: {table_name}")
@@ -24,7 +24,7 @@ for table in tables:
         print(f"  Column: {column[1]}, Type: {column[2]}")
     print()
 
-cursor.execute("SELECT * FROM running")
+cursor.execute("SELECT * FROM HeatMap")
 print(cursor.fetchall())
 
 
--- LiveTracking/dashboard/start.py
+++ LiveTracking/dashboard/start.py
@@ -1,5 +1,5 @@
 import sqlite3
-import time
+import time as t
 from NetRecog import start_func
 from Track_simple import track_simple
 
@@ -12,16 +12,33 @@ def startFunc():
     cursor.execute("DELETE FROM running;")
     cursor.execute("INSERT INTO running (is_running) VALUES (1);")
     conn.commit()
-    conn.close()
+    
 
     time = start_func()
     array = track_simple(time)
-    time_data = time.gmtime(time)
-    month = time_data.tm_mon
-    day = time_data.tm_mday
-    year = time_data.tm_year
+    time_data = t.gmtime(time)
+    month = str(time_data.tm_mon)
+    day = str(time_data.tm_mday)
+    year = str(time_data.tm_year)
+    date = month.rjust(2,'0') + "/" + day.rjust(2,'0') + "/" + year.rjust(4,'0')
     
-    print(array)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM HeatMap WHERE date = ?", (date,))
+    result = cursor.fetchone()
+    cursor.execute("DELETE FROM HeatMap WHERE date = ?", (date,))
+    cursor.execute("INSERT INTO HeatMap VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                   , (  date, 
+                        array[0] + result[1], array[1] + result[2],
+                        array[2] + result[3], array[3] + result[4], 
+                        array[4] + result[5], array[5] + result[6],
+                        array[6] + result[7], array[7] + result[8],
+                        array[8] + result[9], array[9] + result[10],
+                        array[10] + result[11], array[11] + result[12],
+                        array[12] + result[13], array[13] + result[14],
+                        array[14] + result[15], array[15] + result[16],
+                        array[16] + result[17], 0))
+    conn.commit()
+    conn.close()
 
 #start the recording
 # start_func()
