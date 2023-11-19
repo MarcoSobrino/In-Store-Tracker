7463edeb42f708a99ae5a7f718bcebb1f361329d
Walsh Quigley <quiglew@wwu.edu>
2023-11-19 13:39:23 -0800

    Revert "Merge branch 'main' of https://github.com/MarcoSobrino/In-Store-Tracker"
    This reverts commit 67dccf3c5c7a825a68b17b3cbfa195fffa3136fb, reversing
    changes made to 1d859aeb85665dfd074bf538d82a482362a63cd6.

--- LiveTracking/dashboard/HeatMapDataConvert.py
+++ LiveTracking/dashboard/HeatMapDataConvert.py
@@ -10,13 +10,17 @@ def convert_heatmap_data(date):
         print("Error connecting to the database.")
         return
 
+    # Specify the date you want to retrieve
+    # date = '12/01/23'  # Replace with the actual date you want to retrieve
+
     # Query the database to retrieve the data for the specified date
-    query = f"SELECT Date, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P FROM HeatMap WHERE Date = '{date}'"
+    query = f"SELECT * FROM HeatMap WHERE Date = '{date}'"
     df = pd.read_sql_query(query, conn)
 
     # Close the database connection when done
     conn.close()
 
+    # Create a 4x4 DataFrame from the retrieved data
     if not df.empty:
         # Extract the data values from the row
         data_values = df.iloc[0, 1:].values
@@ -25,14 +29,20 @@ def convert_heatmap_data(date):
         data_grid = [data_values[i:i+4] for i in range(0, len(data_values), 4)]
 
         # Create a DataFrame with the 4x4 grid
-        grid_df = pd.DataFrame(data_grid, columns=['A', 'B', 'C', 'D'], index=['d', 'c', 'b', 'a'])
+        grid_df = pd.DataFrame(data_grid)
 
-        # Write the DataFrame to a CSV file
-        grid_df.to_csv('test.csv')
+    # Create a DataFrame with the desired row and column labels
+    row_labels = ['d', 'c', 'b', 'a']
+    col_labels = ['A', 'B', 'C', 'D']
 
-        # Move the CSV file to the HeatMap directory and replace if it already exists
-        os.replace('test.csv', os.path.join(os.path.dirname(__file__), 'test.csv'))
+    grid_df = pd.DataFrame(data_grid, columns=col_labels, index=row_labels)
 
-        print("Success")
+    # Write the DataFrame to a CSV file with the first row as column names then save to same directory as this file
+    grid_df.to_csv('test.csv')
+
+    #move the csv file to the HeatMap directory and replace if already exits
+    os.replace('test.csv', os.path.join(os.path.dirname(__file__), 'test.csv'))
+
+    print("Success")
 
 convert_heatmap_data('12/01/23')
